fe702d33776bce5c3745533583dd33f75406458d
dehanz13 <hansel.danniel@gmail.com>
2022-01-27 23:53:01 -0600

    added deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-tar czf easyio.tar.gz main.js package.json public LICENSE
-scp easyio.tar.gz nanogram@nanogram.io:~
+tar czf easyio.tar.gz main.js package.json yarn.lock public LICENSE
+scp easyio.tar.gz 192.241.131.7:~
 rm easyio.tar.gz
-
-ssh nanogram@nanogram.io << 'ENDSSH'
-pm2 stop all
-rm -rf easyio
-mkdir easyio
-tar xf easyio.tar.gz -C easyio
-rm easyio.tar.gz
-cd easyio 
-npm i
-pm2 start all
-ENDSSH
